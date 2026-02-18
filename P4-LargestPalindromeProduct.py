b4dda37b91a373d777f4f1cd5b23127da50b1512
Christopher Legge <chrislegge151@outlook.com>
2026-02-18 14:08:58 +0000

    committed P4 (not finished)

---
+++ P4-LargestPalindromeProduct.py
@@ -0,0 +1,33 @@
+import math
+
+a = 999
+b = 999
+palendrome = False
+value = 0
+
+def isPaldendrome(num):
+    numArr = list(map(int, str(num)))
+    for i in range(math.floor(len(numArr))):
+        if len(numArr) == 0:
+            return False
+        else:
+            if numArr[0] == numArr[len(numArr)-1]:
+                numArr.pop(0)
+                if len(numArr) != 0:
+                    numArr.pop(len(numArr)-1)
+            else:
+                return False
+    return True
+
+while palendrome == False:
+    if isPaldendrome(a*b):
+        print(a, b, a*b)
+        if a*b > value:
+            value = a*b
+    else:
+        a -= 1
+        if a == 0:
+            b -= 1
+            a = 999
+            if b == 0:
+                palendrome = True
